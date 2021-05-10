66c9eba0ef4e5b1d783234ed137640562a37b739
rashane-murray <rashanemurray@hotmail.com>
2021-05-10 14:21:51 -0500

    Essentials

---
+++ parky/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/theme'
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Logout,
+  LandingPage,
+} from './src/screens'
+
+StatusBar.setBarStyle('light-content')
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Logout" component={Logout} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
---
+++ parky/src/components/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+export default function Logo() {
+  return (
+    <Image
+      source={require('../assets/parkylogo@2x.png')}
+      style={styles.image}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 8,
+  },
+})
